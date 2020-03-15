8473e83a7ea9f165da1b0a613e13a90e127cbd05
Anthony RJ <anthonyjoaquim40@gmail.com>
2020-03-15 13:24:20 +0100

    Finalisation du service d'analyse de fichier XML

--- src/services/ServiceAnalyseXML.java
+++ src/services/ServiceAnalyseXML.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.validation.Validator;
-import javax.validation.ValidatorFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-import com.sun.xml.internal.ws.developer.MemberSubmissionAddressing.Validation;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import bri.Service;
 
-public class ServiceAnalyseXML implements Service {
+public class ServiceAnalyseXML implements Service{
 	
 	private final Socket client;
 	
@@ -25,63 +25,83 @@ public class ServiceAnalyseXML implements Service {
 		client = socket;
 	}
 	
-	// EN CONSTRUCTION !!!!!!!!
 	@Override
-	public void run()  {
+	public void run() {
 		
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-	    Validator validator = factory.getValidator();
 		try {
-		URL[] tabURL = {new URL("ftp://localhost:2121/tp4/lib/"),new URL("ftp://localhost:2121/tp4/fichierXML/")};
+			BufferedReader in = new BufferedReader (new InputStreamReader(client.getInputStream ( )));
+			PrintWriter out = new PrintWriter (client.getOutputStream ( ), true);
+			
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setValidating(false);
+			factory.setNamespaceAware(true);
+			
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+			out.println("Il faut vous connecter à votre mail pour recevoir le rapport");
+			out.println("Seul gmail fonctionne");
+			out.println("Pensez à activer l'option qui permet de recevoir des mails d'application moins sécurisées");
+			out.println("Quel est votre mail ?");
 		
-		URLClassLoader urlcl = new URLClassLoader(tabURL);
-		
-			try {
-				BufferedReader in = new BufferedReader (new InputStreamReader(client.getInputStream ( )));
-				PrintWriter out = new PrintWriter (client.getOutputStream ( ), true);
-				
-				//Etape 1 : Reception du fichier
-				out.println("Indiquez le nom du fichier XML à analyser");
-				try {
-					String fileName = in.readLine();
-					DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-					DocumentBuilder builder = factory.newDocumentBuilder();
-					//Class<?> newFile = urlcl.loadClass(fileName);
-					//InputStream newFile = ClassLoader.getSystemResourceAsStream(fileName);
-					
-					//Ce print est un test
-					//out.println(newFile.getName());
-					
-				} catch (Exception e) {
-					out.println("Erreur : Le fichier indiqué n'existe pas. Vérifiez que le nom ne comporte aucune erreur.");
-				}
-				
-				//Etape 2 : Analyse du fichier
-				
-				
-				
-				
-				
-				//Etape 3 : Envoi de l'analyse par mail via la classe Messagerie
-				
-				
-				
-				
-				
-				
-			} catch (IOException e) {
-			//Fin du service
-			}
-		} catch(Exception e) {
-			}
-		}
-
+			String mail = in.readLine();
+			
+			out.println("Quel est votre mot de passe gmail ?");
+			
+			String mdp = in.readLine();
+			
+			out.println("Indiquer le nom avec extension du fichier xml à analyser");
+			
+			String nomXML = in.readLine();
+			
+			Messagerie mailUser = new Messagerie(mail,mdp);
+			
+			builder.setErrorHandler(new SimpleErrorHandler(mailUser));
+			//Document document = builder.parse(new InputSource("test.xml"));
+			Document document = builder.parse(new InputSource("ftp://localhost:2121/tp4/fichierXML/" + nomXML));
+			
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 	
-	protected void finalize() throws Throwable {
-		 client.close(); 
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
 	}
-
-	public static String toStringue() {
-		return "Inversion de texte";
+	
+	//Le main est là pour tester sans passer par tout le processus d'ajout de service etc
+	/*public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+		validerXML("test.xml");
+		//Messagerie mail = new Messagerie("anthonyjoaquim40@gmail.com","Anthonio123");
+		//mail.envoyerMail("salut");
 	}
+	
+	public static void validerXML(String fichierXML) throws SAXException, IOException, ParserConfigurationException {
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setValidating(false);
+		factory.setNamespaceAware(true);
+		
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		//PrintWriter out = new PrintWriter (client.getOutputStream ( ), true);
+		//out.println("Tapez un texte à inverser");
+		Scanner clavier = new Scanner(System.in);
+		System.out.println("Il faut vous connecter à votre mail pour recevoir le rapport");
+		System.out.println("Seul gmail fonctionne");
+		System.out.println("Pensez à activer l'option qui permet de recevoir des mails d'application moins sécurisées");
+		System.out.println("Quel est votre mail ?");
+		
+		String mail = clavier.nextLine();
+		
+		System.out.println("Quel est votre mdp gmail ?");
+		
+		String mdp = clavier.nextLine();
+		Messagerie mailUser = new Messagerie(mail,mdp);
+		
+		builder.setErrorHandler(new SimpleErrorHandler(mailUser));
+		
+		Document document = builder.parse(new InputSource("ftp://localhost:2121/fichierXML/test.xml"));
+	}*/
 }
